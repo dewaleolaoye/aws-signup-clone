070999299da07a773a9ed617494e55092c5f0751
dewaleolaoye <adewaleolaoye21@gmail.com>
2020-03-20 23:50:07 +0100

    add styles

--- src/components/FormWrapper.tsx
+++ src/components/FormWrapper.tsx
@@ -1,18 +1,38 @@
 import React from 'react';
+import './FormWrapper.scss';
 
 const FormWrapper = () => {
   return (
-    <>
+    <div className='form-wrapper'>
       <form action=''>
-        <label htmlFor='email'>Email</label>
-        <input
-          id='email'
-          name='email'
-          type='email'
-          placeholder='Email address'
-        />
+        <div className='input-wrapper'>
+          <label htmlFor='email'>Email</label>
+          <input id='email' name='email' type='email' required />
+        </div>
+
+        <div className='input-wrapper'>
+          <label htmlFor='password'>Password</label>
+          <input id='password' name='password' type='password' required />
+        </div>
+
+        <div className='input-wrapper'>
+          <label htmlFor='confirmPassword'>Confirm password</label>
+          <input
+            id='confirmPassword'
+            name='confirmPassword'
+            type='password'
+            required
+          />
+        </div>
+
+        <div className='input-wrapper'>
+          <label htmlFor='acct-name'>AWS account name</label>
+          <input id='acct-name' name='acct-name' type='text' required />
+        </div>
+
+        <button>Continue</button>
       </form>
-    </>
+    </div>
   );
 };
 
